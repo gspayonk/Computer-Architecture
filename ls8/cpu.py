27d9bc85e35bcbcb3c697d02ff9f1455d28f7a4f
gspayonk <grissobel.castro@gmail.com>
2020-03-25 12:31:20 -0400

    Day 1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,15 +7,24 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = self.reg[0]
+        self.instruction = {
+            0b00000001: self.HLT,
+            0b10000010: self.LDI,
+            0b01000111: self.PRN
+        }
+
+    def ram_read(self, MAR): #memory address register
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR): #memory data register
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
-
-        # For now, we've just hardcoded a program:
-
         program = [
             # From print8.ls8
             0b10000010, # LDI R0,8
@@ -26,10 +35,23 @@ class CPU:
             0b00000001, # HLT
         ]
 
+
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+        return program
 
+    
+    def HLT(self, op1, op2):
+        return (0, False)
+    
+    def LDI(self, op1, op2):
+        self.reg[op1] = op2
+        return (3, True)
+
+    def PRN(self, op1, op2):
+        print(self.reg[op1])
+        return (2, True)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -62,4 +84,19 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        runs = True
+
+        while runs:
+            inst = self.ram[self.pc]
+            op1 = self.ram_read(self.pc + 1)
+            op2 = self.ram_read(self.pc + 2)
+
+            try:
+                go = self.instruction[inst](op1, op2)
+                self.pc += go[0]
+                runs = go[1]
+
+            except:
+                print('fix input')
+                sys.exit()
+
