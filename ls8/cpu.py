370ddbdf86d0336089bc242c9816a0fdf3fb78bb
gspayonk <grissobel.castro@gmail.com>
2020-03-26 19:46:20 -0400

    mvp day 4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,12 +12,15 @@ class CPU:
         self.reg[self.sp] = 0xF4
         self.halted = False
         self.instruction = {
+            0b10100000: self.ADD,
             0b00000001: self.HLT,
             0b10000010: self.LDI,
             0b01000111: self.PRN,
             0b10100010: self.MUL,
             0b01000101: self.PUSH,
-            0b01000110: self.POP
+            0b01000110: self.POP,
+            0b01010000: self.CALL,
+            0b00010001: self.RET
         }
 
     def ram_read(self, MAR): #memory address register
@@ -50,7 +53,11 @@ class CPU:
                     address += 1
                 else:
                     continue
-    
+                
+    def ADD(self):
+        self.alu('ADD', self.ram_read(self.pc+1),self.ram_read(self.pc+2))
+        self.pc += 3
+
     def HLT(self):
         self.halted = True
         self.pc += 1
@@ -78,6 +85,14 @@ class CPU:
     def POP(self):
         self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.reg[self.sp])
         self.pc += 2
+        self.reg[self.sp] += 1
+
+    def CALL(self):
+        self.PUSH(self.pc+2)
+        self.pc = self.reg[self.ram_read(self.pc-1)]
+
+    def RET(self):
+        self.pc = self.ram_read(self.reg[self.sp])
         self.reg[self.sp] += 1
 
     def alu(self, op, reg_a, reg_b):
